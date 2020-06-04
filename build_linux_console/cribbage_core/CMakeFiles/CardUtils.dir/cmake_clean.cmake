c7ba5ad33155fdbd057b2363d7cce8e15c68c272
Sean Igo <samwibatt@gmail.com>
2020-06-03 20:18:45 -0700

    simple worky cmake

---
+++ build_linux_console/cribbage_core/CMakeFiles/CardUtils.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CardUtils.dir/card_utils.cpp.o"
+  "libCardUtils.pdb"
+  "libCardUtils.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CardUtils.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
