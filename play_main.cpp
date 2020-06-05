194a3dd955f23feee856b5c419496de6d44c72d1
Sean Igo <samwibatt@gmail.com>
2020-06-05 08:32:53 -0700

    removed build dirs

--- play_main.cpp
+++ play_main.cpp
@@ -3,6 +3,11 @@
 #include "cribbage_core.h"
 #include "cribbage_player.h"
 
+//what if we just had globals
+CardUtils cu;
+Cribbage cr;
+CribbagePlayer cp[2];
+
 int main(int argc, char *argv[]) {
     printf("Hello and welcome to Cribbulation, the crinkly cribbage playing utility.\n");
     return 0;
