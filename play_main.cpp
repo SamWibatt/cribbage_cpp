0862c2867a0ff7c2088150c517dd84aa64fdbea1
Sean Igo <samwibatt@gmail.com>
2020-06-06 20:55:50 -0700

    platspec main and printf work

--- play_main.cpp
+++ play_main.cpp
@@ -3,13 +3,15 @@
 #include "cribbage_core.h"
 #include "cribbage_player.h"
 #include "plat_io.h"
+#include "plat_play_main.h"
 
 //what if we just had globals
 CardUtils cu;
 Cribbage cr;
 CribbagePlayer cp[2];
 
-int main(int argc, char *argv[]) {
-    plprintf("Hello and welcome to Cribbulation, the crinkly cribbage playing utility.\n");
-    return 0;
+//platform-specific main will call this after doing platspec setup
+void run() {
+    std::string gamename = "Cribbulationzzz";
+    plprintf("Hello and welcome to %s, the crinkly cribbage playing utility.\n",gamename.c_str());
 }
