814e3db8b7f2fc8b8d1147e1ee8a4c48b03a8388
Sean Igo <samwibatt@gmail.com>
2020-06-14 21:58:12 -0700

    minimax mostly written

--- minimax_main.cpp
+++ minimax_main.cpp
@@ -7,52 +7,166 @@
 #include "minimax_main.h"
 #include "plat_minimax_main.h"
 
-/*
-Right so
-Let us bull on and write this, like I did with the Python implementations! I've gotten ossified.
-Go nuts and have fun! Celebrate the fact that I have at least a basic knowledge of C++11, CMake,
-Google test, and code coverage! CELEBRATE! LEARN! APPLY!
-
-So here is code for minimax with alpha-beta. From wikipedia!
-
-function alphabeta(node, depth, α, β, maximizingPlayer) is
-    if depth = 0 or node is a terminal node then
-        return the heuristic value of node
-    if maximizingPlayer then
-        value := −∞
-        for each child of node do
-            value := max(value, alphabeta(child, depth − 1, α, β, FALSE))
-            α := max(α, value)
-            if α ≥ β then
-                break (* β cut-off *)
-        return value
-    else
-        value := +∞
-        for each child of node do
-            value := min(value, alphabeta(child, depth − 1, α, β, TRUE))
-            β := min(β, value)
-            if β ≤ α then
-                break (* α cut-off *)
-        return value
-
-(* Initial call *)
-alphabeta(origin, depth, −∞, +∞, TRUE)
+CardUtils cu;
+Cribbage cr;
 
 
-*/
+namespace minimax {
+  /*
+  Right so
+  Let us bull on and write this, like I did with the Python implementations! I've gotten ossified.
+  Go nuts and have fun! Celebrate the fact that I have at least a basic knowledge of C++11, CMake,
+  Google test, and code coverage! CELEBRATE! LEARN! APPLY!
 
-//CribbageCountNode implementations
-node_value_t CribbageCountNode::heuristicValue() {
-  //WRITE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  //WRITE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  //WRITE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  //WRITE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  //WRITE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  //WRITE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  //WRITE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  return 0;
+  So here is code for minimax with alpha-beta. From wikipedia!
+
+  function alphabeta(node, depth, α, β, maximizingPlayer) is
+      if depth = 0 or node is a terminal node then
+          return the heuristic value of node
+      if maximizingPlayer then
+          value := −∞
+          for each child of node do
+              value := max(value, alphabeta(child, depth − 1, α, β, FALSE))
+              α := max(α, value)
+              if α ≥ β then
+                  break (* β cut-off *)
+          return value
+      else
+          value := +∞
+          for each child of node do
+              value := min(value, alphabeta(child, depth − 1, α, β, TRUE))
+              β := min(β, value)
+              if β ≤ α then
+                  break (* α cut-off *)
+          return value
+
+  (* Initial call *)
+  alphabeta(origin, depth, −∞, +∞, TRUE)
+
+  in our case it's
+  index_t maxd = 9; 
+  set_max_depth(maxd);
+  alphabeta(root, maxd, min_node_value, max_node_value, true)
+  */
+
+  node_value_t MinimaxRunner::alphabeta(MinimaxNode &node, index_t depth, node_value_t alpha, node_value_t beta, bool is_max) {
+    node_value_t val;
+
+    // if depth = 0 or node is a terminal node then
+    //     return the heuristic value of node
+    if(depth == 0 || node.is_terminal()) { return node.heuristic_value(); }
+
+    // slightly confused here - if this is a max node, the children are min nodes & vv, yes? hence !is_max
+    std::vector<MinimaxNode> children = node.find_legal_countermoves(!is_max);
+
+    // if maximizingPlayer then
+    if(is_max) {
+      //     value := −∞
+      val = min_node_value; 
+
+      //     for each child of node do
+      //         value := max(value, alphabeta(child, depth − 1, α, β, FALSE))
+      //         α := max(α, value)
+      //         if α ≥ β then
+      //             break (* β cut-off *)
+      for(MinimaxNode n : children) {
+        // do we need to pass alpha and beta back? I'm going to try not.
+        val = std::max(alpha, alphabeta(n, depth-1, alpha, beta, false));
+        alpha = std::max(alpha, val);
+        if(alpha >= beta) break;    //beta cutoff
+      }
+
+      //     return value
+      return val; 
+    } else {
+      //     value := +∞
+      val = max_node_value;
+
+      //     for each child of node do
+      //         value := min(value, alphabeta(child, depth − 1, α, β, TRUE))
+      //         β := min(β, value)
+      //         if β ≤ α then
+      //             break (* α cut-off *)
+      for(MinimaxNode n : children) {
+        // do we need to pass alpha and beta back? I'm going to try not.
+        val = std::min(beta, alphabeta(n, depth-1, alpha, beta, true));
+        alpha = std::min(alpha, val);
+        if(alpha >= beta) break;    //beta cutoff
+      }
+    }
+    //shouldn't be reachable, but this shuts compiler warning up
+    return val;
+  }
+
+
+  //CribbageCountNode implementations =================================================================================================
+  CribbageCountNode::CribbageCountNode() {
+    //ctor stuff
+  }
+
+  CribbageCountNode::~CribbageCountNode() {
+    //dtor stuff
+  }
+
+  bool CribbageCountNode::is_terminal() {
+    //what makes a terminal node? When a node is terminal, we return its heuristic value,
+    //so there needs to be a card to play and no cards left in the hand.
+    //return handcards.empty();
+    //wait, rethink. That's still true, but not the only condition
+    //this is always called before heuristic_value, so we could have other conditions;
+    //could do the call to play_card here and if it's an error, then what?
+    //let's say that's a terminal node. That doesn't let us play all the way to the end,
+    //just to 31, and... let's go with that
+
+    //we have a card to play, a stack to play it on.
+    //we don't want score lists, do we want to add to stack? Prolly not
+    index_t playscore = cr.play_card(stackcards,card_to_play,nullptr,false,false);  
+
+    //so if that isn't a legal move, this is a terminal node, no change to cumulative score
+    if(playscore == cr.ERROR_SCORE_VAL) return true;
+
+    //HERE DO SOMETHING WITH CUMULATIVE_SCORE...............
+    //add if we're maxing, subtract if we're minning?
+    //or does that even make sense?
+    //FIGURE THIS OUT ************************************************************************
+    //make it so that returning cumulative value is the heuristic value
+    //we have to know if this is a max or min node. if max, add to cumulative
+    //if min, subtract from it
+    if(max_node) {
+      cumulativeScore += playscore;
+    } else {
+      cumulativeScore -= playscore;
+    }
+
+    //otherwise, if there are no cards left in the hand, it's a terminal node
+    if(handcards.empty()) return true;
+
+    return false;
+  }
+
+  node_value_t CribbageCountNode::heuristic_value() {
+    //ok! So what's the value of a cribbage count node?
+    //since is_terminal did the calc, cumulativeScore is all we need
+    return cumulativeScore;
+  }
+
+  std::vector<MinimaxNode> CribbageCountNode::find_legal_countermoves(bool is_max) {
+    std::vector<MinimaxNode> rv;
+
+    //OK HERE IS THE LAST BIT I NEED TO WRITE
+    if(is_max) {
+      // it's a max node, we're generating the min nodes that follow.
+      // so... hm. what's the equivalent to handcards for the min case? We need to step through
+      // that. It is: opponentcards!
+    } else {
+      // it's a min no de, generate the max nodes that follow.
+      // iterate through handcards and create nodes that have each one as card_to_play
+      // and that card removed from handcards
+    }
+
+    return rv;
+  }
 }
-
 
 // main method ------------------------------------------------------------------------------------------
 void run() {
--- minimax_main.h
+++ minimax_main.h
@@ -10,68 +10,123 @@
 #include "plat_minimax_main.h"
 
 
+namespace minimax {
+  // types
+  //node_value_t should be a signed type
+  typedef int32_t node_value_t;
 
-// types
-typedef int32_t node_value_t;
+  // constants
+  // set these to be the most positive and most negative values possible, they're the +/- infinity for alpha and beta
+  const node_value_t min_node_value = INT32_MIN;
+  const node_value_t max_node_value = INT32_MAX;
 
-class CribbageCountNode {
-  protected:
-    //what goes in a node?
-    //depth, as in move/countermove depth. Can be no more than 8.
-    index_t depth;
+  class MinimaxNode {
 
-    // cumulative score leading up to this point...? I guess so. If we can carry along the cards
-    // and stack, and could carry along the previous board states in Othello, we can carry this and not be cheating
-    node_value_t cumulativeScore;
+    protected:
+      //depth, as in move/countermove depth.
+      index_t depth;
 
-    // alpha and beta - are these a node thing or just in the caller?
-    node_value_t alpha, beta;
 
-    //stack so far - in terms of ranks. Is it misleading to use card_t?
-    //no, let's ignore suit by considering everything to have suit 0, hearts.
-    //that way, they will still work with the cardutils val() and rank() and therefore
-    //the cribbage_core play_card.
-    std::vector<card_t> stackcards;
+    public:
+      MinimaxNode() {}
+      virtual ~MinimaxNode() {} 
 
-    //cards in hand
-    std::vector<card_t> handcards;
+      //accessors ----------------------------------------------------------------------------------------------
+      index_t get_depth() { return depth; }
+      void set_depth(index_t d) { depth = d; }
 
-    //ranks of cards remaining in deck - this is how we can impose the constraint that there be only 4
-    //cards of each rank represented. initialize to all 4s, then subtract 1 as a card of that rank
-    //is used in either dealing the max player's hand or proposing a min player's countermove.
-    //SO IF I WANT TO TRY A TOY VERSION, HOW ABOUT HAVING THE OPPONENT'S HAND IN HERE RATHER THAN THE 
-    //WHOLE REST OF THE DECK?
-    std::array<index_t,13> remainingRankCounts;
+    public:
+      virtual node_value_t heuristic_value() { return 0; }
+      virtual bool is_terminal() { return true; }
+      
+      //find all the legal countermoves to this state - should it be all *possible* countermoves?
+      virtual std::vector<MinimaxNode> find_legal_countermoves(bool is_max) { 
+        return std::vector<MinimaxNode>();
+      }
 
-  public:
-    // accessors ---------------------------------------------------------------------------------------
-    index_t get_depth() { return depth; }
-    void set_depth(index_t d) { depth = d; }
+  };
 
-    node_value_t get_cumulative_score() { return cumulativeScore; }
-    void set_cumulative_score(node_value_t s) { cumulativeScore = s; }
+  class MinimaxRunner {
+    protected:
+      //maximum depth of descent, huge or known larger than max possible depth for unlimited
+      index_t max_depth = 0;
 
-    node_value_t get_alpha() { return alpha; }
-    void set_alpha(node_value_t s) { alpha = s; }
+    public:
+      MinimaxRunner() {}
+      virtual ~MinimaxRunner() {}
 
-    node_value_t get_beta() { return beta; }
-    void set_beta(node_value_t s) { beta = s; }
+      index_t get_max_depth() { return max_depth; }
+      void set_max_depth(index_t nm) { max_depth = nm; }
 
-    //do we want a ref here?
-    std::vector<card_t> &get_stackcards() { return stackcards; }
-    void set_stackcards(std::vector<card_t> sc) { stackcards = sc; }
+    public:
+      virtual node_value_t alphabeta(MinimaxNode &node, index_t depth, node_value_t alpha, node_value_t beta, bool is_max);
+  };
 
-    //do we want a ref here?
-    std::vector<card_t> &get_handcards() { return handcards; }
-    void set_handcards(std::vector<card_t> sc) { handcards = sc; }
+  class CribbageCountNode : MinimaxNode {
+    protected:
+      //what goes in a node?
+      //it has to know if it's a max or min for score evaluation
+      bool max_node;
 
-    //do we want a ref here?
-    std::array<index_t,13> get_remaining_rank_counts() { return remainingRankCounts; }
-    void set_remaining_rank_counts(std::array<index_t,13> rrc) { remainingRankCounts = rrc; }
+      // cumulative score leading up to this point...? I guess so. If we can carry along the cards
+      // and stack, and could carry along the previous board states in Othello, we can carry this and not be cheating
+      node_value_t cumulativeScore;
 
-    // actions -----------------------------------------------------------------------------------------
-    //heuristic value should be signed to allow for negative numbers - some states might be really baddddd
-    node_value_t heuristicValue();
-};
+      //stack so far - in terms of ranks. Is it misleading to use card_t?
+      //no, let's ignore suit by considering everything to have suit 0, hearts.
+      //that way, they will still work with the cardutils val() and rank() and therefore
+      //the cribbage_core play_card.
+      std::vector<card_t> stackcards;
+
+      //cards in hand after the one being played, for finding children
+      std::vector<card_t> handcards;
+
+      //card being played
+      card_t card_to_play;
+
+      //ranks of cards remaining in deck - this is how we can impose the constraint that there be only 4
+      //cards of each rank represented. initialize to all 4s, then subtract 1 as a card of that rank
+      //is used in either dealing the max player's hand or proposing a min player's countermove.
+      //SO IF I WANT TO TRY A TOY VERSION, HOW ABOUT HAVING THE OPPONENT'S HAND IN HERE RATHER THAN THE 
+      //WHOLE REST OF THE DECK?
+      //or how about those are the same thing? 
+      //let's just call it opponentcards - of course, it'll be just hearts
+      std::vector<card_t> opponentcards;
+
+    public:
+      // ctor / dtor -------------------------------------------------------------------------------------
+
+      CribbageCountNode();
+      virtual ~CribbageCountNode();
+
+      // accessors ---------------------------------------------------------------------------------------
+      bool is_max_node() { return max_node; }
+      void set_max_node(bool numax) { max_node = numax; }
+
+      node_value_t get_cumulative_score() { return cumulativeScore; }
+      void set_cumulative_score(node_value_t s) { cumulativeScore = s; }
+
+      //do we want a ref here?
+      std::vector<card_t> &get_stackcards() { return stackcards; }
+      void set_stackcards(std::vector<card_t> sc) { stackcards = sc; }
+
+      //do we want a ref here?
+      std::vector<card_t> &get_handcards() { return handcards; }
+      void set_handcards(std::vector<card_t> sc) { handcards = sc; }
+
+      card_t get_card_to_play() { return card_to_play; }
+      void set_card_to_play(card_t ctp) { card_to_play = ctp; }
+
+      //do we want a ref here?
+      std::vector<card_t> &get_opponentcards() { return opponentcards; }
+      void set_opponentcards(std::vector<card_t> oc) { opponentcards = oc; }
+
+      // actions -----------------------------------------------------------------------------------------
+      //heuristic value should be signed to allow for negative numbers - some states might be really baddddd
+      node_value_t heuristic_value() override;
+      bool is_terminal() override;
+      std::vector<MinimaxNode> find_legal_countermoves(bool is_max) override;
+  };
+}
 
 #endif
